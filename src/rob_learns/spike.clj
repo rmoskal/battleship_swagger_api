d630716880fc3d8182b264d0a4cc69c23e9cf6e8
rmoskal <rmoskal@mostmedia.com>
2018-02-16 10:46:31 -0500

    learing

--- src/rob_learns/spike.clj
+++ src/rob_learns/spike.clj
@@ -1,8 +1,27 @@
-(defn fib-step [[a b]]
-  [b (+ a b)])
+(defn _foo
+  [arr]
+  (loop [working arr res []]
+    (if (empty? working) res
+                         (if (= (first working) (second working)) (recur (drop 2 working) res)
+                                                                  (recur (rest working) (conj res (first working)))))
+    ))
 
-(defn fib-seq []
-  (map first (iterate fib-step [0 1])))
 
-(println(take 20 (fib-seq)))
+(defn foo
+  [arr]
+  (loop [current arr]
+    (println current)
+    (if (apply distinct? current) current
+                                  (recur (_foo current)))
+    ))
+
+
+
+(let [in (slurp *in*)]
+  (println (clojure.string/split in #"")))
+
+
+
+
+
 
