127abfc9ec1e98803e7938e62dd8b72ef0f01942
rmoskal <rmoskal@mostmedia.com>
2018-10-10 19:06:27 -0400

    made a rest api cause I'm that sort of guy.

--- project.clj
+++ project.clj
@@ -4,13 +4,9 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [failjure "1.2.0"]
-                 [metosin/compojure-api "1.1.11"]
-                 [ring/ring-jetty-adapter "1.4.0"]
-                 [environ "1.0.0"]]
+                 [metosin/compojure-api "1.1.11"]]
   :ring {:handler my-api.handler/app}
   :uberjar-name "server.jar"
-  :plugins [[environ/environ.lein "0.3.1"]]
-  :hooks [environ.leiningen.hooks]
   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
                                   [cheshire "5.5.0"]
                                   [ring/ring-mock "0.3.0"]]
