7d65fd12cd2da60e2553ec0275e0c0917b4527f0
rmoskal <rmoskal@mostmedia.com>
2018-10-10 19:10:23 -0400

    made a rest api cause I'm that sort of guy.

--- src/my_api/handler.clj
+++ src/my_api/handler.clj
@@ -6,3 +6,50 @@
 
 
 
+
+(def app
+  (api
+    {:swagger
+     {:ui   "/"
+      :spec "/swagger.json"
+      :data {:info {:title       "Who Sunk My Battleship!?"
+                    :description "Alloyed Test Api"}
+             :tags [{:name "api", :description "Battleship game for two!"}]}}}
+
+    (context "/api" []
+      :tags ["api"]
+
+      (GET "/plus" []
+        :return {:result Long}
+        :query-params [x :- Long, y :- Long]
+        :summary "adds two numbers together"
+        (ok {:result (+ x y)}))
+
+      (POST "/init" []
+        (initialize-game)
+        :summary "Initializes the game!"
+        (ok {:player1 @player_1 :player2 @player_2})
+        )
+      (POST "/place/:player/:piece/:direction" []
+        :summary "Places a piece on the board"
+        :path-params [player :- (s/enum :player1 :player2),
+                      piece :- (s/enum "submarine" "destroyer" "cruiser" "battleship" "carrier"),
+                      direction :- (s/enum :v :h)]
+        :query-params [x :- Long, y :- Long]
+        (let [player (get-atom player)]
+          (ok {:player (place-move player piece (direction directionfn) x y)})))
+      (GET "/:player" []
+        :summary "Gets the state for a player"
+        :path-params [player :- (s/enum :player1 :player2)]
+        (let [player (get-atom player)]
+          (ok {:player @player})))
+      (POST "/attack/:attacker/:attacked" []
+        :summary "First player attacks the second"
+        :path-params [attacker :- (s/enum :player1 :player2),
+                      attacked :- (s/enum :player1 :player2)]
+        :query-params [x :- Long, y :- Long]
+
+        (let [[attacker attacked] [(get-atom attacker) (get-atom attacked)]]
+          (ok {:player (attack-move attacker attacked x y)})
+          )
+        ))))
