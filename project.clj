12cc31badf9b94f6fffd4c6e11436ae413bcfdf2
rmoskal <rmoskal@mostmedia.com>
2018-10-10 19:18:02 -0400

    made a rest api cause I'm that sort of guy.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [failjure "1.2.0"]
                  [metosin/compojure-api "1.1.11"]]
   :ring {:handler my-api.handler/app}
+  :min-lein-version "2.0.0"
   :uberjar-name "server.jar"
   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
                                   [cheshire "5.5.0"]
