79242d83b0761c6a3d549ae4757d7dde8d5be94f
Robert Moskal <rmoskal@mostmedia.com>
2018-11-12 12:23:20 -0500

    Update handler.clj
    Made the spacing nice, but maybe not idiomatic.

--- src/my_api/handler.clj
+++ src/my_api/handler.clj
@@ -4,9 +4,6 @@
             [rob-learns.game-server :refer :all]
             [schema.core :as s]))
 
-
-
-
 (def app
   (api
     {:swagger
@@ -15,10 +12,9 @@
       :data {:info {:title       "Who Sunk My Battleship!?"
                     :description "MostMedia Test Api"}
              :tags [{:name "api", :description "Battleship game for two!"}]}}}
-
     (context "/api" []
       :tags ["api"]
-
+             
       (GET "/plus" []
         :return {:result Long}
         :query-params [x :- Long, y :- Long]
@@ -30,6 +26,7 @@
         :summary "Initializes the game!"
         (ok {:player1 @player_1 :player2 @player_2})
         )
+             
       (POST "/place/:player/:piece/:direction" []
         :summary "Places a piece on the board"
         :path-params [player :- (s/enum :player1 :player2),
@@ -38,17 +35,18 @@
         :query-params [x :- Long, y :- Long]
         (let [player (get-atom player)]
           (ok {:player (place-move player piece (direction directionfn) x y)})))
+             
       (GET "/:player" []
         :summary "Gets the state for a player"
         :path-params [player :- (s/enum :player1 :player2)]
         (let [player (get-atom player)]
           (ok {:player @player})))
+             
       (POST "/attack/:attacker/:attacked" []
         :summary "First player attacks the second"
         :path-params [attacker :- (s/enum :player1 :player2),
                       attacked :- (s/enum :player1 :player2)]
         :query-params [x :- Long, y :- Long]
-
         (let [[attacker attacked] [(get-atom attacker) (get-atom attacked)]]
           (ok {:player (attack-move attacker attacked x y)})
           )
